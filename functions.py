c80bf9c4c91d35c297cbc06010bd3483b2a09850
opilane <opilane@HP-CZC2349HTB>
2025-04-28 11:32:50 +0300

    1

--- functions.py
+++ functions.py
@@ -1,22 +1,11 @@
-# import ast
-# def ast_read():
-#     jarjend=[]
-#     with open('kontakt.txt',encoding="utf8") as f: 
-#         data = f.read() 
-#         o=ast.literal_eval(data)
-#         for rida in f:
-#             rida=ast.literal_eval(rida)
-#             jarjend.append((rida.strip()))
-#     return jarjend
-
+#
 def Loe_failist(fail:str)->list:
     f=open(fail,'r',encoding="utf8")
-    s=0
+
     jarjend=[]
     for rida in f:
         jarjend.append(rida.strip())
-        dict(jarjend[s])
-        s+=1
+
     f.close()
     print(type(jarjend[0]))
     return jarjend
@@ -27,12 +16,7 @@ def Kirjuta_failisse(fail:str,jarjend1:list,jarjend2:list,jarjend3:list):
     sõnastik=[]
     for e, r, g in zip(jarjend1,jarjend2, jarjend3):
         sõnastik.append({'nimi': e, 'email': r, 'number': g})
-    # for line in jarjend1:
-    #     f.write(str(line)+":")
-    # for line in jarjend2:
-    #     f.write(str(line)+":")
-    # for line in jarjend3:
-    #     f.write(str(line)+":")
+
     for line in sõnastik:
         f.write(str(line)+"\n")
     f.close()
